a3b1ec6c790e7577edecd20bd45fba0e94fb2f2b
Jaron Johnson <jjohnson@Jarons-MacBook-Pro.local>
2021-01-13 21:26:47 -0700

    Have almost working template prototype

--- main.js
+++ main.js
@@ -2,3 +2,64 @@ require('dotenv').config()
 const axios = require('axios');
 const csvtojson = require('csvtojson');
 const fs = require('fs');
+const inquirer = require('inquirer');
+
+////////// Change these up ///////////////
+const token = process.env.TOKEN;
+const baseUrl = 'jjohnson.com';
+const preAPIUrl = `users`;
+const postAPIUrl = 'enrollments'
+const apiType = 'get';
+const apiData = {
+
+};
+//////////////////////////////////////////
+
+const instance = axios.create({
+    baseURL: `https://${baseUrl}/api/`,
+    timeout: 1000,
+    headers: { 'Authorization': `Bearer ${token}` }
+});
+
+function main() {
+    const csvStorage = fs.readdirSync('./csv-storage');
+    console.log("🚀 ~ file: main.js ~ line 26 ~ main ~ csvStorage", csvStorage)
+    try {
+        inquirer.prompt({
+                message: "What file do you want to use?",
+                name: "file",
+                choices: csvStorage,
+                type: 'list'
+            })
+            .then(res => {
+                const returnJson = {};
+                csvtojson()
+                    .fromFile(res.file)
+                    .then(fileJSON => {
+                        console.log("🚀 ~ file: main.js ~ line 39 ~ main ~ fileJSON", fileJSON)
+
+                        fileJSON.forEach(object => {
+                            instance({
+                                    method: apiType,
+                                    url: `${preAPIUrl}/${object}/${postAPIUrl}`,
+                                    data: apiData
+                                })
+                                .then(res => {
+                                    returnJson[object] = res.data;
+                                })
+                        });
+                    })
+                fs.writeFile('returnData.json', JSON.stringify(returnJson, null, 2), (err) => {
+                    if (err) {
+                        throw err
+                    } else {
+                        console.log(`File has been saved to returnData.json`)
+                    }
+                });
+            })
+    } catch (err) {
+        throw err;
+    }
+}
+
+main();
